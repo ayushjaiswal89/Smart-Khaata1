8f5f9ad954089dbabd631bf7e3be1a50bba5359c
ayushjaiswal89 <jaiswalayush06725@gmail.com>
2025-11-30 16:48:38 +0530

    Refactor service worker installation and caching

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,13 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("smart-khaata-v1").then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js",
-        "/icon-192.png",
-        "/icon-512.png",
-
-        "/manifest.json"
+        "./",               // homepage
+        "index.html",
+        "style.css",
+        "script.js",
+        "icon-192.png",
+        "icon-512.png",
+        "manifest.json"
       ]);
     })
   );

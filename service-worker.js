ae1df748e9827b966200b14ad2d6215d12979f8b
ayushjaiswal89 <jaiswalayush06725@gmail.com>
2025-11-30 16:32:50 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", e => {
+  console.log("SW Installed");
+  e.waitUntil(
+    caches.open("smart-khaata-v1").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/icon-192.png",
+        "/icon-512.png",
+
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
